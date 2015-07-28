5c05251507dbdb44fde7988593da6a73b82b6188
Jeremy Raines <jraines@gmail.com>
2015-07-28 16:33:41 -0400

    Initial challenges

---
+++ methods.rb
@@ -0,0 +1,19 @@
+
+def sum_array(array)
+  #find the sum of the items in the array
+  #it's possible that nil items could be
+  #members of the array
+end
+
+def titleize(string)
+  #capitalizes the first letter of every word in a string
+  #except an, a, of, the.  Unless that word is the first word
+  #of the string
+end
+
+def interpose(array1, array2)
+  #returns a nested array, where each nested array is the corresponding
+  #two elements of the two give arrays
+  #For example: given arrays [1, 2, 3] and [:a, :b, :c]
+  #return [[1, :a], [2, :b], [3, :c]]
+end
