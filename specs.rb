f102c0813fea2bf007647427c9beef550231363f
Jeremy Raines <jraines@gmail.com>
2015-07-28 17:39:42 -0400

    Solutions for first problems

--- specs.rb
+++ specs.rb
@@ -1,1 +1,37 @@
-require 'methods.rb'
+require './methods.rb'
+
+RSpec.configure do |config|
+  config.color = true
+end
+
+describe '#sum_array' do
+  it 'should sum up the array, ignoring nils' do
+    arr = [nil, nil, 1, 2, nil, 4]
+    expect(sum_array(arr)).to eq(7)
+  end
+end
+
+describe '#titleize' do
+  it 'should put a string into title case' do
+    str = "a dream of spring"
+    expect(titleize(str)).to eq('A Dream of Spring')
+  end
+
+  it 'It should downcase any non-capitalizable words if they come in as capitals' do
+    str = "a dream of SPRING"
+    expect(titleize(str)).to eq('A Dream of Spring')
+  end
+end
+
+describe '#interpose' do
+
+  it 'interposes two arrays by element' do
+    a1 = [1, 2, 3]
+    a2 = [:a, :b, :c]
+
+    expect(interpose(a1, a2)).to eq([[1, :a],
+                                     [2, :b],
+                                     [3, :c]])
+  end
+
+end
